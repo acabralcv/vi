5c03762e60df13a2db4dea44b3e3e5f94640be95
Osvanildo <osvanildotn@gmail.com>
2019-06-07 21:27:22 -0100

    Merge branch 'osvanildo&hamilto' of http://github.com/acabralcv/vi
    # Conflicts:
    #	application/src/main/java/com/app/controllers/HomeController.java

---
+++ application/src/main/java/com/app/controllers/CadeiasController.java
@@ -0,0 +1,18 @@
+package com.app.controllers;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CadeiasController {
+
+    @RequestMapping(value = "cadeias", method = RequestMethod.GET)
+    public String actionIndex(ModelMap model, @PageableDefault(sort = {"name"}, value = 10, page = 0) Pageable pageable) {
+
+        return  "/views/cadeia/index";
+    }
+}
