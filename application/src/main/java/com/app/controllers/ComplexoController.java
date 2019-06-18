1f1e53093a0f8fcb41357995fd873c406a3d8f19
Osvanildo <osvanildotn@gmail.com>
2019-06-18 17:14:54 -0100

    osvaldino updates v3

--- application/src/main/java/com/app/controllers/ComplexoController.java
+++ application/src/main/java/com/app/controllers/ComplexoController.java
@@ -1,8 +1,7 @@
 package com.app.controllers;
 
-import com.library.models.Complexo;
+
 import com.app.helpers.ServiceProxy;
-import com.app.service.ComplexoService;
 import com.app.service.CadeiaService;
 import com.library.helpers.BaseResponse;
 import com.library.helpers.HelperPaging;
@@ -12,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -22,7 +22,68 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.ArrayList;
 
+@Controller
 public class ComplexoController {
 
+
+
+    @Autowired
+    private Environment env;
+
+    /**
+     *
+     * @param model
+     * @param pageable
+     * @return
+     */
+    @RequestMapping(value = "admin/complexos", method = RequestMethod.GET)
+    public String actionIndex(ModelMap model, @PageableDefault(sort = { "name"}, value = 10, page = 0) Pageable pageable) {
+
+
+        ServiceProxy oServiceProxy = new ServiceProxy(env);
+        //get info
+        BaseResponse objResponse = oServiceProxy
+                .getJsonData("api/complexos", oServiceProxy.encodePageableParams(pageable));
+        oServiceProxy.close();
+
+        //Pageable result objt
+        JSONObject dataResponse = (JSONObject) objResponse.getData();
+        if(dataResponse != null) {
+        //check result
+        ArrayList<Complexo> Listacomplexos = (ArrayList<Complexo>) dataResponse.get("content");
+
+            model.addAttribute("objPaging", (new HelperPaging().getResponsePaging(pageable, dataResponse)));
+            model.addAttribute("complexos", Listacomplexos);
+            }
+        return  "/views/complexo/index";
+    }
+
+
+
+    @RequestMapping(value = {"admin/complexo/create"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String actionCreate(@Valid @ModelAttribute Complexo objComplexo, BindingResult result,
+                               ModelMap model, HttpServletRequest request) {
+
+        if (request.getMethod().equals("POST")) {
+
+            if (!result.hasErrors()){
+
+                ServiceProxy oServiceProxy = new ServiceProxy(env);
+                BaseResponse oBaseResponse = oServiceProxy.postJsonData("api/complexos/create", objComplexo, new ArrayList<>() );
+                oServiceProxy.close();
+
+                Complexo createdCadeia = (Complexo) BaseResponse.convertToModel(oBaseResponse, new Complexo());
+
+                if(createdCadeia != null)
+                    return "redirect:/admin/complexo"; // + createdCadeia.getId();
+                else
+                    throw new InternalError(oBaseResponse.getMessage());
+            }
+        }
+
+        model.addAttribute("objComplexo", objComplexo);
+        //model.addAttribute("cadeiaList", new CadeiaService(env).findOne());
+        return "views/complexos/create";
+    }
 
 }
