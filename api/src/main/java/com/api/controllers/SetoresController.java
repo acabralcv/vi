f9c46d1180e30efd5dbb2779f8a938c28aa17f35
Osvanildo <osvanildotn@gmail.com>
2019-06-25 12:14:40 -0100

    osvaldino updates osvanildo&hamilton

--- api/src/main/java/com/api/controllers/SetoresController.java
+++ api/src/main/java/com/api/controllers/SetoresController.java
@@ -3,6 +3,7 @@ package com.api.controllers;
 import com.library.helpers.BaseResponse;
 import com.library.helpers.Helper;
 import com.library.helpers.UtilsDate;
+import com.library.models.Complexo;
 import com.library.models.Setor;
 import com.library.repository.SetorRepository;
 import com.library.repository.UserRepository;
@@ -23,55 +24,55 @@ public class SetoresController {
 
 
 
-//    @Autowired
-//    private Environment env;
-//
-//    @Autowired
-//    private SetorRepository setorRepository;
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @RequestMapping(value = {"api/setores"}, method = {RequestMethod.GET})
-//    public ResponseEntity actionDetails(@PageableDefault(sort = {"nome"}, value = 10, page = 0) Pageable pageable) {
-//
-//        try  {
-//
-//            Pageable pageableBuilded = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("nome")
-//                    .ascending()
-//                    .and(Sort.by("dateCreated").descending()));
-//
-//            Page<Setor> setores = setorRepository.findByStatus(Helper.STATUS_ACTIVE, pageableBuilded);
-//
-//            return ResponseEntity.ok().body(new BaseResponse().getObjResponse(1,"ok", setores ));
-//
-//        }catch (Exception e){
-//            new EventsLogService(userRepository).AddEventologs(null,"Excption in class '" + this.getClass().getName()
-//                    + "' method " + Thread.currentThread().getStackTrace()[1].getMethodName() + "()",e.getMessage(),null, null);
-//
-//            return ResponseEntity.ok().body(new BaseResponse(0, e.getMessage(), null));
-//        }
-//    }
-//
-//    @RequestMapping(value = {"api/setores/create"}, method = {RequestMethod.POST})
-//    public ResponseEntity actionCreate(@RequestBody Setor setor) {
-//
-//        try {
-//
-//            setor.setId(new Helper().getUUID());
-//            setor.setStatus(Helper.STATUS_ACTIVE);
-//            setor.setDateCreated(UtilsDate.getDateTime());
-//
-//            setor crestedSetor = setorRepository.save(setor);
-//
-//            return ResponseEntity.ok().body(new BaseResponse().getObjResponse(1,"ok", crestedSetor ));
-//
-//        }catch (Exception e){
-//            new EventsLogService(userRepository).AddEventologs(null,"Excption in class '" + this.getClass().getName()
-//                    + "' method " + Thread.currentThread().getStackTrace()[1].getMethodName() + "()",e.getMessage(),null, null);
-//            return ResponseEntity.ok().body(new BaseResponse(0, e.getMessage(), null));
-//        }
-//    }
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private SetorRepository setorRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping(value = {"api/setores"}, method = {RequestMethod.GET})
+    public ResponseEntity actionDetails(@PageableDefault(sort = {"nome"}, value = 10, page = 0) Pageable pageable) {
+
+        try  {
+
+            Pageable pageableBuilded = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("nome")
+                    .ascending()
+                    .and(Sort.by("dateCreated").descending()));
+
+            Page<Setor> setores = setorRepository.findByStatus(Helper.STATUS_ACTIVE, pageableBuilded);
+
+            return ResponseEntity.ok().body(new BaseResponse().getObjResponse(1,"ok", setores ));
+
+        }catch (Exception e){
+            new EventsLogService(userRepository).AddEventologs(null,"Excption in class '" + this.getClass().getName()
+                    + "' method " + Thread.currentThread().getStackTrace()[1].getMethodName() + "()",e.getMessage(),null, null);
+
+            return ResponseEntity.ok().body(new BaseResponse(0, e.getMessage(), null));
+        }
+    }
+
+    @RequestMapping(value = {"api/setores/create"}, method = {RequestMethod.POST})
+    public ResponseEntity actionCreate(@RequestBody Setor setor) {
+
+        try {
+
+            setor.setId(new Helper().getUUID());
+            setor.setStatus(Helper.STATUS_ACTIVE);
+            setor.setDateCreated(UtilsDate.getDateTime());
+
+            Setor crestedSetor = setorRepository.save(setor);
+
+            return ResponseEntity.ok().body(new BaseResponse().getObjResponse(1,"ok", crestedSetor ));
+
+        }catch (Exception e){
+            new EventsLogService(userRepository).AddEventologs(null,"Excption in class '" + this.getClass().getName()
+                    + "' method " + Thread.currentThread().getStackTrace()[1].getMethodName() + "()",e.getMessage(),null, null);
+            return ResponseEntity.ok().body(new BaseResponse(0, e.getMessage(), null));
+        }
+    }
 
 
 }
